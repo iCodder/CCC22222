69170d948da0e7236fd344a851a8a05fc409ccaa
PBPII <PBPII@PBPII-PC.Home>
2014-11-26 16:47:41 +0000

    The new class

---
+++ src/gitHub/testGitHub.java
@@ -0,0 +1,45 @@
+package gitHub;
+
+public class testGitHub {
+	
+	int a;
+	int b;
+	String name;
+	
+
+	public int getA() {
+		return a;
+	}
+
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+
+	public int getB() {
+		return b;
+	}
+
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
