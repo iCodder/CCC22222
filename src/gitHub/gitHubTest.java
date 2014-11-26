ccfe61cfaa777e53229dcf2bcaea4f3d8cdf9337
PBPII <PBPII@PBPII-PC.Home>
2014-11-26 14:41:24 +0000

    Testing

---
+++ src/gitHub/gitHubTest.java
@@ -0,0 +1,38 @@
+package gitHub;
+
+public class gitHubTest {
+	public static void main(String [] args){
+		
+		int odd = 1;
+		int noOfSpaces = 4;
+		for (int i = 1; i <= 4; i++) {
+			
+			int k = 0;
+			for (int j = 1; j < noOfSpaces; j++) {
+				System.out.print(".");
+			}
+			
+			for (int j = 1; j <= odd; j++) {
+				
+				
+				if(j <= i)
+				{
+					k = k + 1;
+				}
+				
+				else
+				{
+					k = k - 1;
+				}
+			
+				System.out.print(k);
+			}
+			
+			System.out.println();
+			odd = odd + 2;
+			noOfSpaces = noOfSpaces - 1;
+			
+		}
+		
+	}
+}
